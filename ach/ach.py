caf5f2a60025193da2725273e804fab2e6c406f4
Travis Hathaway <travis.j.hathaway@gmail.com>
2012-10-17 23:34:18 -0700

    got a start on the AchHeader class

--- ach/ach.py
+++ ach/ach.py
@@ -27,3 +27,34 @@ class Ach(object):
             zero_string += '0'
 
         return zero_string
+
+class AchHeader(Ach):
+    """
+    Creates our File Header record of the nacha file
+    """
+
+    def __init__(self):
+        """
+        Initializes all values needed for 
+        our header row
+        """
+
+        self.__record_type_code = record_type_code
+        self.__priority_code    = priority_code
+        self.__immediate_dest   = immediate_dest
+        self.__immediate_org    = immediate_org
+        self.__file_crt_date    = self.__create_date()
+        self.__file_crt_time    = self.__create_time()
+        self.__file_id_mod      = file_id_mod
+        self.__record_size      = record_size
+        self.__blk_factor       = blk_factor
+        self.__format_code      = format_code
+        self.__im_dest_name     = im_dest_name
+        self.__im_orgn_name     = im_orgn_name
+        self.__reference_code   = reference_code
+
+    def __create_date(self):
+        return '120502'
+
+    def __create_time(self):
+        return '1234'
