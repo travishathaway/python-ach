89b028c27c061cf9658c5dff9c3a6bc5ffd8ab09
Travis Hathaway <travis.j.hathaway@gmail.com>
2014-07-14 18:10:29 -0700

    adding new version information

---
+++ CHANGES.md
@@ -0,0 +1,11 @@
+# python-ach changelog
+
+
+### 0.2 2014-07-14
+- Added support for addenda records
+- Added file parser
+- Bug fixes 
+
+### 0.1 2014-07-14
+- Initial release
+- Support for PPD and CCD records without addenda record type
--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from distutils.core import setup 
+from distutils.core import setup
 
 setup(
     name='ach',
     author='Travis Hathaway',
     author_email='travis.j.hathaway@gmail.com',
-    version='0.1',
+    version='0.2',
     packages=[
         'ach',
     ],
     url='https://github.com/travishathaway/python-ach',
     license='MIT License',
+    description='Library to create and parse ACH files (NACHA)',
     long_description=open('README.md').read(),
 )
